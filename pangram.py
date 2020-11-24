202ea7c2aaa702c0b62b99ba885d0d4e1ed8490e
Matt Forgione <mathew_forgione@msn.com>
2020-11-23 20:15:28 -0400

    first and last commit

--- pangram.py
+++ pangram.py
@@ -1,26 +1,14 @@
 import string
 
 def is_pangram(s):
-    stringArr = []
-    count = 0
-    for i in range(26):
-        stringArr.append(string.ascii_letters[i])
+    # Create list from string using list comprehension
+    my_list = [x for x in s]
+    # Equates to new_list.append(i.lower()) using branching logic at the end of
+    # the list comp expression, this is less efficient but more readable and compact.
+    new_list = [x.lower() for x in my_list if x in string.ascii_lowercase or x in string.ascii_uppercase]
+    # Remove duplicates from new arr and copy to final testable list by instantiating a new list object 
+    final_list = list(set(new_list))
 
-    for i in range(len(s)):
-        if s[i] in stringArr:
-            count += 1
-            print(s[i])
-            continue
-        else:
-            return False
-    return True
-
-    # if s in stringArr:
-    #     return 'Bingo!'
-    # else:
-    #     print(stringArr)
-    #     return 'Not a bingo.'
-
-print(is_pangram('abcdefghijklmnopqrstuvwxyz'))
-
-# 'abcdefghijklmnopqrstuvwxyz'
+    if len(final_list) >= 26:
+        return True
+    return False
